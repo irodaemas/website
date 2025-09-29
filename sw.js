9322e732a1cd7fb46f8002d4bdc142418619c624
Admin <admin@mail.com>
2025-09-29 21:51:11 +0700

    update

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Sentral Emas – Service Worker (subfolder-friendly) */
-const CACHE_NAME = 'sentralemas-v12';
+const CACHE_NAME = 'sentralemas-v13';
 const FONT_CACHE = 'sentralemas-fonts-v2';
 
 // Core assets gunakan path relatif terhadap scope
